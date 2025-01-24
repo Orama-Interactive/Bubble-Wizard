c2b00454885a2dee79bd719cd6beb0cfbff2000c
OverloadedOrama <manoschool@yahoo.gr>
2025-01-24 20:26:00 +0200

    Add bubble movement code

--- src/player.gd
+++ src/player.gd
@@ -9,11 +9,51 @@ extends CharacterBody2D
 @export_range(0, 20) var gravity_scale := 2.0
 ## Your player will move this amount faster when falling providing a less floaty jump curve.
 @export_range(0.5, 3) var descending_gravity_factor := 1.1
+@export var terminal_velocity := 400.0
+@export_category("Bubble form")
+@export var bubble_duration := 2.0
+@export var bubble_speed := 120.0
+@export var bubble_vertical_speed := 1.0
+@export var bubble_decelerate := 10.0
+@export var bubble_gravity_scale := 0.4
 var can_jump := false
 var jump_pressed := false
+var bubble_form := false
 
 
 func _physics_process(delta: float) -> void:
+	if bubble_form:
+		_bubble_movement(delta)
+	else:
+		_normal_movement(delta)
+
+
+func _bubble_movement(delta: float) -> void:
+	#var direction := Input.get_vector(&"left", &"right", &"up", &"down")
+	var direction_horiz :=  Input.get_axis(&"left", &"right")
+	if direction_horiz:
+		velocity.x = direction_horiz * bubble_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, bubble_decelerate)
+		#velocity.y = move_toward(velocity.y, 0, bubble_decelerate)
+
+	var direction_vert := Input.get_axis(&"up", &"down")
+	#if direction_vert:
+		#velocity.y = direction_vert * bubble_speed
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, bubble_decelerate)
+	var gravity := get_gravity() * delta * bubble_gravity_scale
+	if direction_vert:
+		#gravity = get_gravity() * delta * bubble_gravity_scale * direction_vert * ver
+		gravity *= direction_vert * bubble_vertical_speed
+	velocity += gravity
+	velocity.y = clampf(velocity.y, -terminal_velocity, terminal_velocity)
+	#print(direction_horiz, " ", velocity)
+	#print(gravity, " ", velocity)
+	move_and_slide()
+
+
+func _normal_movement(delta: float) -> void:
 	# Add the gravity.
 	if is_on_floor():
 		can_jump = true
@@ -42,6 +82,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
+	velocity.y = clampf(velocity.y, -terminal_velocity, terminal_velocity)
 	move_and_slide()
 
 

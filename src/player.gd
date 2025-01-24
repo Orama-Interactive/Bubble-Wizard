12f45c8acdd3416863068a79cc3174da0b0b818d
OverloadedOrama <manoschool@yahoo.gr>
2025-01-24 22:08:30 +0200

    Implement fall damage and improve bubble charging

--- src/player.gd
+++ src/player.gd
@@ -9,22 +9,25 @@ extends CharacterBody2D
 @export_range(0, 20) var gravity_scale := 2.0
 ## Your player will move this amount faster when falling providing a less floaty jump curve.
 @export_range(0.5, 3) var descending_gravity_factor := 1.1
-@export var terminal_velocity := 400.0
+@export var terminal_velocity := 1200.0
+@export var fall_damage_velocity := 700.0
 @export_category("Bubble form")
 @export var bubble_speed := 120.0
 @export var bubble_vertical_speed := 1.0
 @export var bubble_decelerate := 10.0
 @export var bubble_gravity_scale := 0.2
 @export var bubble_duration := 2.0
-@export var bubble_charge_time := 1.2
+@export var bubble_charge_time := 0.6
 var can_jump := false
 var jump_pressed := false
 var bubble_form := false:
 	set(value):
 		bubble_form = value
 		if bubble_form:
+			velocity.y = 0
 			can_jump = true
 			bubble_timer.start()
+var last_vertical_velocity := 0.0
 
 @onready var bubble_timer: Timer = $BubbleTimer
 @onready var charge_bubble_timer: Timer = $ChargeBubbleTimer
@@ -32,6 +35,7 @@ var bubble_form := false:
 
 func _ready() -> void:
 	bubble_timer.wait_time = bubble_duration
+	charge_bubble_timer.wait_time = bubble_charge_time
 
 
 func _physics_process(delta: float) -> void:
@@ -60,8 +64,12 @@ func _normal_movement(delta: float) -> void:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	if not is_zero_approx(direction):
+		if not charge_bubble_timer.is_stopped():
+			charge_bubble_timer.stop()
 
 	velocity.y = clampf(velocity.y, -terminal_velocity, terminal_velocity)
+	last_vertical_velocity = velocity.y
 	move_and_slide()
 
 
@@ -106,6 +114,10 @@ func _jump_buffer() -> void:
 	jump_pressed = false
 
 
+func _handle_death() -> void:
+	print("YOU DIED")
+
+
 func _charge_bubble_form() -> void:
 	if Input.is_action_just_pressed("charge_bubble"):
 		charge_bubble_timer.start()
@@ -126,4 +138,6 @@ func _on_charge_bubble_timer_timeout() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == self:
 		return
+	if not bubble_form and last_vertical_velocity > fall_damage_velocity:
+		_handle_death()
 	bubble_form = false

1f7d3050c7c9276d31cf43b7f07eb0ddad75be92
OverloadedOrama <manoschool@yahoo.gr>
2025-01-25 23:11:45 +0200

    Set keybinding tutorial strings in code

---
+++ src/Levels/level_2.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var bubble_form_tutorial_label: Label = $BubbleFormTutorialLabel
+
+
+func _ready() -> void:
+	var charge_str := InputMap.action_get_events("charge_bubble")[0].as_text().replace("(Physical)", "")
+	bubble_form_tutorial_label.text = bubble_form_tutorial_label.text % charge_str

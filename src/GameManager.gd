2862ce42af0c0565416946883231be9a760cbc1e
OverloadedOrama <manoschool@yahoo.gr>
2025-01-26 17:41:09 +0200

    Toggle fullscreen

--- src/GameManager.gd
+++ src/GameManager.gd
@@ -17,6 +17,15 @@ func _ready() -> void:
 	TranslationServer.set_locale(OS.get_locale())
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+
 func start_changing_level() -> void:
 	var scene_transition_rect := get_tree().current_scene.find_child("SceneTransitionRect") as SceneTransitionRect
 	scene_transition_rect.animation_player.play(&"fade_in")

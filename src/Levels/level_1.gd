113426015d0e10f1d2d6211e3b4f60ef54af358e
OverloadedOrama <manoschool@yahoo.gr>
2025-01-26 14:27:32 +0200

    Fix level tutorial labels

--- src/Levels/level_1.gd
+++ src/Levels/level_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var arrow_keys_tutorial_label: Label = $ArrowKeysTutorialLabel
 @onready var jump_tutorial_label: Label = $JumpTutorialLabel
+@onready var jump_tutorial_label2: Label = $JumpTutorialLabel2
 
 
 func _ready() -> void:
@@ -12,3 +13,4 @@ func _ready() -> void:
 		tr(arrow_keys_tutorial_label.text) % [left_string, right_string]
 	)
 	jump_tutorial_label.text = tr(jump_tutorial_label.text) % [jump_string]
+	jump_tutorial_label2.text = tr(jump_tutorial_label2.text) % [jump_string]

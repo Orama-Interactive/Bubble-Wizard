2564c783a556e16fda98c3018894581573dd9947
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-01-27 23:17:11 +0200

    Add touch screen buttons for jumping and casting bubble

--- src/scene_transition_rect.gd
+++ src/scene_transition_rect.gd
@@ -11,3 +11,11 @@ func _ready() -> void:
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == &"fade_in":
 		GameManager.change_level()
+
+
+func _on_touch_screen_button_pressed() -> void:
+	GameManager.mouse_movement_enabled.emit(false)
+
+
+func _on_touch_screen_button_released() -> void:
+	GameManager.mouse_movement_enabled.emit(true)
